07c1843eaf5d510797ece36e6b8192f822512a03
CanadianDoc <jun.kang223@hotmail.com>
2023-12-01 22:00:30 -0700

    ready.js fix

--- src/events/bot/ready.js
+++ src/events/bot/ready.js
@@ -4,7 +4,6 @@ module.exports = {
   name: "ready",
   once: true,
   async execute(bot) {
-    await connectToMongoDB();
     console.log(`${bot.user.tag} online`);
     bot.user.setPresence({
       activities: [{ name: `over the chaos`, type: ActivityType.Watching }],
